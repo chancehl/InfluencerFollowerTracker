93c16539bad6747fd0e389914035f38b418c21f8
chancehl <clinz.dev@gmail.com>
2024-02-18 23:23:44 -0800

    🔧 refactor(InstagramAccountRepository.java): change InstagramAccountRepository ID type from UUID to String for better compatibility with Instagram's ID format

--- src/main/java/com/chancehl/InfluencerFollowerTracker/repositories/InstagramAccountRepository.java
+++ src/main/java/com/chancehl/InfluencerFollowerTracker/repositories/InstagramAccountRepository.java
@@ -3,6 +3,4 @@ package com.chancehl.InfluencerFollowerTracker.repositories;
 import com.chancehl.InfluencerFollowerTracker.models.InstagramAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface InstagramAccountRepository extends JpaRepository<InstagramAccount, UUID> {}
+public interface InstagramAccountRepository extends JpaRepository<InstagramAccount, String> {}
