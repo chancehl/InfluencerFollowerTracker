e3794c92397c544c94964ff76b8d7dffd92665fb
chancehl <clinz.dev@gmail.com>
2024-02-18 23:37:25 -0800

    adds comments explaining the HACKY_ASS_USER_AGENT_FROM_INTERNET variable

--- src/main/java/com/chancehl/InfluencerFollowerTracker/services/InstagramAccountService.java
+++ src/main/java/com/chancehl/InfluencerFollowerTracker/services/InstagramAccountService.java
@@ -22,6 +22,12 @@ public class InstagramAccountService {
     @Autowired
     private InstagramAccountRepository instagramAccountRepository;
 
+    // Don't ask me why, but this is absolutely required in order to call their API.
+    // We can omit this by swapping to the Instagram Graph API, but that only works for
+    // fetching the followers of creator, business, and verified accounts. I'm assuming
+    // that some of the people on these shows won't have verified accounts at first.
+    // So, for now, I'm going to keep using the "private" API that Meta exposes to their
+    // mobile clients. Those APIs require a mobile User-Agent header.
     private static final String HACKY_ASS_USER_AGENT_FROM_INTERNET = "Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890; en_US; 138226743)";
 
     public InstagramPrivateApiUserResponse getFollowerCount(String name) throws IOException, InterruptedException {
